f03087f8bd1f3a95796e50afa3a1f3223e955285
Andrew Glaws <andrew.glaws@nrel.gov>
2021-02-08 16:16:30 -0700

    Add TFRecord generator

--- utils.py
+++ utils.py
@@ -126,3 +126,71 @@ def plot_SR_data(idx, LR, SR, path):
 
         plt.savefig(path+'/img{0:05d}.png'.format(idx[i]), dpi=200, bbox_inches='tight')
         plt.close()
+
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+def downscale_image(x, K):
+    tf.reset_default_graph()
+
+    if x.ndim == 3:
+        x = x.reshape((1, x.shape[0], x.shape[1], x.shape[2]))
+
+    x_in = tf.placeholder(tf.float64, [None, x.shape[1], x.shape[2], x.shape[3]])
+
+    weight = tf.constant(1.0/K**2, shape=[K, K, x.shape[3], x.shape[3]], dtype=tf.float64)
+    downscaled = tf.nn.conv2d(x_in, filter=weight, strides=[1, K, K, 1], padding='SAME')
+
+    with tf.Session() as sess:
+        ds_out = sess.run(downscaled, feed_dict={x_in: x})
+
+    return ds_out
+
+def generate_TFRecords(filename, data, mode='test', K=None):
+    '''
+        Generate TFRecords files for model training or testing
+
+        inputs:
+            filename - filename for TFRecord (should by type *.tfrecord)
+            data     - numpy array of size (N, h, w, c) containing data to be written to TFRecord
+            model    - if 'train', then data contains HR data that is coarsened k times 
+                       and both HR and LR data written to TFRecord
+                       if 'test', then data contains LR data 
+            K        - downscaling factor, must be specified in training mode
+
+        outputs:
+            No output, but .tfrecord file written to filename
+    '''
+    if mode == 'train':
+        assert K is not None, 'In training mode, downscaling factor K must be specified'
+        data_LR = downscale_image(data, K)
+
+    with tf.python_io.TFRecordWriter(filename) as writer:
+        for j in range(data.shape[0]):
+            if mode == 'train':
+                h_HR, w_HR, c = data[j, ...].shape
+                h_LR, w_LR, c = data_LR[j, ...].shape
+                features = tf.train.Features(feature={
+                                     'index': _int64_feature(j),
+                                   'data_LR': _bytes_feature(data_LR[j, ...].tostring()),
+                                      'h_LR': _int64_feature(h_LR),
+                                      'w_LR': _int64_feature(w_LR),
+                                   'data_HR': _bytes_feature(data[j, ...].tostring()),
+                                      'h_HR': _int64_feature(h_HR),
+                                      'w_HR': _int64_feature(w_HR),
+                                         'c': _int64_feature(c)})
+            elif mode == 'test':
+                h_LR, w_LR, c = data[j, ...].shape
+                features = tf.train.Features(feature={
+                                     'index': _int64_feature(j),
+                                   'data_LR': _bytes_feature(data[j, ...].tostring()),
+                                      'h_LR': _int64_feature(h_LR),
+                                      'w_LR': _int64_feature(w_LR),
+                                         'c': _int64_feature(c)})
+
+            example = tf.train.Example(features=features)
+            writer.write(example.SerializeToString()) 
