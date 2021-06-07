24f9e3437168aa85477ff4e33b1ea81b28f877f1
Sebastian Hoffmann <shoffmann.321@gmail.com>
2021-06-07 15:51:38 +0200

    adjusted to CNHW, converted to tf2

--- era5_to_tfrecords.py
+++ era5_to_tfrecords.py
@@ -4,20 +4,17 @@ import hdf5plugin
 import dask.array as da
 import numpy as np
 
-
-# TF v1 Script!
+from dask.diagnostics import ProgressBar
 
 
 class Downscaler:
 
-    def __init__(self, sess, factor, img_shape):
-        self.sess = sess
+    def __init__(self, factor, img_shape):
         self.factor = factor
-        self.x_in = tf.placeholder(tf.float32, [None] + list(img_shape))
-        self.pool = tf.nn.avg_pool2d(self.x_in, [1, factor, factor, 1], [1, factor, factor, 1],  padding='SAME')
 
+    @tf.function
     def downscale(self, x):
-        return self.sess.run(self.pool, feed_dict={self.x_in: x})
+        return tf.nn.avg_pool2d(x, [1, self.factor, self.factor, 1], [1, self.factor, self.factor, 1],  padding='SAME')
 
 
 def _bytes_feature(value):
@@ -38,7 +35,7 @@ def patchify(data, y, x, patch_size):
     return slices
 
 
-def patchify_random(data, patch_size, n_patches=14):
+def patchify_random(data, patch_size, n_patches):
     h,w = patch_size
 
     y = np.random.randint(0, data.shape[1] - h, size=(data.shape[0], n_patches))
@@ -49,7 +46,7 @@ def patchify_random(data, patch_size, n_patches=14):
     return patches, y, x
 
 
-def generate_TFRecords(writer, data, downscaler, mode, HR_patches):
+def generate_TFRecords(writer, data, downscaler, mode, HR_patches, n_patches):
     '''
     copied from utils.py and modified to allow appending
     '''    
@@ -59,7 +56,7 @@ def generate_TFRecords(writer, data, downscaler, mode, HR_patches):
         data_LR = downscaler.downscale(data)
 
     if HR_patches:
-        data, y, x = patchify_random(data, HR_patches)
+        data, y, x = patchify_random(data, HR_patches, n_patches)
         if mode == 'train':
             K = downscaler.factor
             h,w = HR_patches[0] // K, HR_patches[1] // K
@@ -73,10 +70,10 @@ def generate_TFRecords(writer, data, downscaler, mode, HR_patches):
 
             features = tf.train.Features(feature={
                                     'index': _int64_feature(j),
-                                'data_LR': _bytes_feature(data_LR[j].tostring()),
+                                'data_LR': _bytes_feature(data_LR[j].tobytes()),
                                     'h_LR': _int64_feature(h_LR),
                                     'w_LR': _int64_feature(w_LR),
-                                'data_HR': _bytes_feature(data[j].tostring()),
+                                'data_HR': _bytes_feature(data[j].tobytes()),
                                     'h_HR': _int64_feature(h_HR),
                                     'w_HR': _int64_feature(w_HR),
                                         'c': _int64_feature(c)})
@@ -85,7 +82,7 @@ def generate_TFRecords(writer, data, downscaler, mode, HR_patches):
 
             features = tf.train.Features(feature={
                                     'index': _int64_feature(j),
-                                'data_LR': _bytes_feature(data[j].tostring()),
+                                'data_LR': _bytes_feature(data[j].tobytes()),
                                     'h_LR': _int64_feature(h_LR),
                                     'w_LR': _int64_feature(w_LR),
                                         'c': _int64_feature(c)})
@@ -97,10 +94,10 @@ def generate_TFRecords(writer, data, downscaler, mode, HR_patches):
 def main():
     ########################################################
     
-    infile = '/data2/era5/ds_train_1980_1994.hdf5'
-    outfile = '/data/stengel/patches/patches_train_1980_1994.{}.tfrecords'
+    infile = '/data/ERA5/hdf5_hr/ds_train_1979_to_1990.hdf5'
+    outfile = 'patches_train_1979_1990.{}.tfrecords'
     n_files = 8
-    gzip = False
+    gzip = True
     shuffle = True
 
     ########################################################
@@ -109,47 +106,50 @@ def main():
     log1p_norm = True
     z_norm = False
     
-    mean, std = [275.28983, 1.8918675e-08, 2.3001131e-07], [16.951859, 2.19138e-05, 4.490682e-05]
-    mean_log1p, std_log1p = [0.034322508, 0.01029128, 0.0031989873], [0.6344424, 0.53678083, 0.54819226]
+    mean, std = [2.0152406e-08, 2.1581373e-07], [2.8560082e-05, 5.0738556e-05]
+    mean_log1p, std_log1p = [0.008315503, 0.0028762482], [0.5266841, 0.5418187]
 
-    ################### #####################################
+    #########################################################
 
-    HR_reduce_latitude = 32  # 11.25 deg from each pole
+    HR_reduce_latitude = 107  # 15 deg from each pole
     HR_patches = (96, 96)
+    n_patches = 50
 
     ########################################################
 
-
     f = h5py.File(infile, 'r', rdcc_nbytes=1000*1000*1000)
-    data = da.from_array(f['data'], chunks=(16 if shuffle else 256, -1, -1, -1))
+    data = da.from_array(f['data'], chunks=(-1, 16 if shuffle else 256, -1, -1))  # CNHW layout
+    data = da.transpose(data, (1,2,3,0))
     dtype = data.dtype
 
     if dtype != np.float32:
         print('WARNING: data will be saved as float32 but input ist float64!')
 
     if mean is None:
-        mean, std = da.compute(data.mean(axis=[0,1,2]), data.std(axis=[0,1,2]))
+        arr = data
+        with ProgressBar():
+            mean, std = da.compute(arr.mean(axis=[0,1,2]), arr.std(axis=[0,1,2]), num_workers=8)
     else:
         mean, std = np.asarray(mean, dtype=dtype), np.asarray(std, dtype=dtype)
+
+    print('mean: {}, std: {}'.format(list(mean), list(std)))
 
     if log1p_norm:
         data_z_norm = (data-mean) / std
         data_log1p = da.sign(data_z_norm) * da.log1p(da.fabs(data_z_norm))
 
         if mean_log1p is None:
-            mean_log1p, std_log1p = da.compute(data_log1p.mean(axis=[0,1,2]), data_log1p.std(axis=[0,1,2]))
+            arr = data_log1p
+            with ProgressBar():
+                mean_log1p, std_log1p = da.compute(arr.mean(axis=[0,1,2]), arr.std(axis=[0,1,2]), num_workers=8)
         else:
             mean_log1p, std_log1p = np.asarray(mean_log1p, dtype=dtype), np.asarray(std_log1p, dtype=dtype)
 
-        print('mean: {}, std: {}'.format(list(mean), list(std)))
         print('mean_log1p: {}, std_log1p: {}'.format(list(mean_log1p), list(std_log1p)))
 
         data = data_log1p
     elif z_norm:
         data = (data-mean) / std
-
-    if not log1p_norm:
-        print('mean: {}, std: {}'.format(list(mean), list(std)))
 
     if shuffle:
         block_indices = np.random.permutation(data.numblocks[0])
@@ -158,34 +158,33 @@ def main():
 
 
     # Create session and start writing
-    with tf.Session() as sess:
-        if HR_reduce_latitude:
-            H,W,C = data.shape[1:]
-            downscaler = Downscaler(sess, SR_ratio, (H-HR_reduce_latitude, W, C))
+    if HR_reduce_latitude:
+        H,W,C = data.shape[1:]
+        downscaler = Downscaler(SR_ratio, (H-HR_reduce_latitude, W, C))
+    else:
+        downscaler = Downscaler(SR_ratio, data.shape[1:])
+
+    file_blocks = np.array_split(block_indices, n_files)
+    i = 0
+    for n, indices in enumerate(file_blocks):
+        if n_files > 1:
+            name = outfile.format(n)
         else:
-            downscaler = Downscaler(sess, SR_ratio, data.shape[1:])
+            name = outfile
+        
+        with tf.io.TFRecordWriter(name, options='ZLIB' if gzip else None) as writer:
+            for block_idx in indices:
+                block =  data.blocks[block_idx].compute()
+                if shuffle:
+                    block = np.random.permutation(block)
 
-        file_blocks = np.array_split(block_indices, n_files)
-        i = 0
-        for n, indices in enumerate(file_blocks):
-            if n_files > 1:
-                name = outfile.format(n)
-            else:
-                name = outfile
-            
-            with tf.python_io.TFRecordWriter(name, options='ZLIB' if gzip else None) as writer:
-                for block_idx in indices:
-                    block =  data.blocks[block_idx].compute()
-                    if shuffle:
-                        block = np.random.permutation(block)
+                if HR_reduce_latitude:
+                    lat_start = HR_reduce_latitude//2
+                    block = block[:, lat_start:(-lat_start),:, :]
 
-                    if HR_reduce_latitude:
-                        lat_start = HR_reduce_latitude//2
-                        block = block[:, lat_start:(-lat_start),:, :]
-
-                    generate_TFRecords(writer, block, downscaler, 'train', HR_patches)
-                    i += 1
-                    print('{} / {}'.format(i, data.numblocks[0]))
+                generate_TFRecords(writer, block, downscaler, 'train', HR_patches, n_patches)
+                i += 1
+                print('{} / {}'.format(i, data.numblocks[0]))
 
 if __name__ == '__main__':
     main()
