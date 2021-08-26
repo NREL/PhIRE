bbbd2d2feed76693c745923ae37c18e5eacfecd3
Sebastian Hoffmann <shoffmann.321@gmail.com>
2021-08-26 11:01:24 +0200

    data tool supports phire and rplearn now

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 console_scripts = [
-    'rplearn-data=phire.rplearn.data_tool:main',
+    'rplearn-data=phire.data_tool:rplearn_main',
     'rplearn-train=phire.rplearn.train:main',
     'phire-train=phire.main:main',
     'phire-eval=phire.evaluation.cli:main',
-    'phire-data=phire.era5_to_tfrecords:main'
+    'phire-data=phire.data_tool:phire_main'
 ]
 
 
